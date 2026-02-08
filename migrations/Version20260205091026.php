f9a4d57e7da714cfa83ffcb1efef12b2da172b40
AhmadouDIAW <ahmadoudiawdiaw@gmail.com>
2026-02-08 01:05:46 +0100

    Page d'accueil avec navbar et dropdown Connexion fonctionnel, boutons vers actualités

--- migrations/Version20260205091026.php
+++ migrations/Version20260205091026.php
@@ -7,25 +7,24 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20260205091026 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Création de la table categorie';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE categorie (id INT AUTO_INCREMENT NOT NULL, libelle VARCHAR(255) NOT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('CREATE TABLE categorie (
+            id INT AUTO_INCREMENT NOT NULL,
+            libelle VARCHAR(255) NOT NULL,
+            PRIMARY KEY (id)
+        ) DEFAULT CHARACTER SET utf8mb4');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE categorie');
     }
 }
