e19a65e07a031ab44f48c79151a8684d52833b5b
apierre1541 <axelle.pierre@btsinfo-aurillac.fr>
2026-02-11 10:45:46 +0100

    Modification affichage code dans le controller RegistrationController.php

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -14,11 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class RegistrationController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register')]
-    public function register(
-        Request $request,
-        EntityManagerInterface $entityManager,
-        UserPasswordHasherInterface $passwordHasher
-    ): Response {
+    public function register(Request $request,EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher ): Response {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
 
