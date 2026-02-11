7e926de394007d14faee35b3e836ba72ee470197
apierre1541 <axelle.pierre@btsinfo-aurillac.fr>
2026-02-11 13:07:49 +0100

    Modification du fichier PresentationEntrepriseCrud.php pour le coté admin pour avoir tout les champs

--- src/Controller/Admin/PresentationEntrepriseCrudController.php
+++ src/Controller/Admin/PresentationEntrepriseCrudController.php
@@ -4,9 +4,12 @@ namespace App\Controller\Admin;
 
 use App\Entity\PresentationEntreprise;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TelephoneField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 
 class PresentationEntrepriseCrudController extends AbstractCrudController
 {
@@ -15,14 +18,19 @@ class PresentationEntrepriseCrudController extends AbstractCrudController
         return PresentationEntreprise::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            ImageField::new('logo')->setColumns(12)->setBasePath('/presentation_entreprise')->setUploadDir('/public/presentation_entreprise'),
+            ImageField::new('image')->setColumns(12)->setBasePath('/presentation_entreprise')->setUploadDir('/public/presentation_entreprise'),
+            TextEditorField::new('service_proposee')->setColumns(12),
+            EmailField::new('email')->setColumns(12)->setHelp('Format : nom@domaine.fr'),
+            TelephoneField::new('telephone')->setColumns(12)->setHelp('Format: +33 X XX XX XX XX'),
+            TextField::new('adresse_postal')->setColumns(12),
+            IntegerField::new('code_postal')->setColumns(12)->setHelp('Format: XXXXX'),
+            TextField::new('commune')->setColumns(12),
         ];
     }
-    */
+    
 }
