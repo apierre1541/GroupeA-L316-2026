c538fa8c055254f5fc67f827269f3cbb71628c9c
Youssef Badri <badriyoussef75@gmail.com>
2026-02-11 23:58:24 +0100

    changer la migration Version20260210143156 pour eviter les erreurs lors de démarage (IF EXISTS / IF NOT EXISTS)

--- migrations/Version20260210143156.php
+++ migrations/Version20260210143156.php
@@ -19,17 +19,43 @@ final class Version20260210143156 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, nom VARCHAR(100) NOT NULL, prenom VARCHAR(100) NOT NULL, telephone VARCHAR(20) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
-        $this->addSql('DROP TABLE sequelizemeta');
-        $this->addSql('DROP TABLE users');
+        
+        $this->addSql('CREATE TABLE IF NOT EXISTS user (
+            id INT AUTO_INCREMENT NOT NULL,
+            email VARCHAR(180) NOT NULL,
+            roles JSON NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            nom VARCHAR(100) NOT NULL,
+            prenom VARCHAR(100) NOT NULL,
+            telephone VARCHAR(20) NOT NULL,
+            UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email),
+            PRIMARY KEY (id)
+        ) DEFAULT CHARACTER SET utf8mb4');
+    
+
+        $this->addSql('DROP TABLE IF EXISTS sequelizemeta');
+        $this->addSql('DROP TABLE IF EXISTS users');
     }
 
+    
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE sequelizemeta (name VARCHAR(255) CHARACTER SET utf8mb3 NOT NULL COLLATE `utf8mb3_unicode_ci`, UNIQUE INDEX name (name), PRIMARY KEY (name)) DEFAULT CHARACTER SET utf8mb3 COLLATE `utf8mb3_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, firstname VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, lastname VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, createdAt DATETIME DEFAULT \'CURRENT_TIMESTAMP(6)\' NOT NULL, updatedAt DATETIME DEFAULT \'CURRENT_TIMESTAMP(6)\' NOT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('DROP TABLE user');
+        $this->addSql('CREATE TABLE IF NOT EXISTS sequelizemeta (
+            name VARCHAR(255) CHARACTER SET utf8mb3 NOT NULL COLLATE `utf8mb3_unicode_ci`,
+            UNIQUE INDEX name (name),
+            PRIMARY KEY (name)
+        ) DEFAULT CHARACTER SET utf8mb3 COLLATE `utf8mb3_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+    
+        $this->addSql('CREATE TABLE IF NOT EXISTS users (
+            id INT AUTO_INCREMENT NOT NULL,
+            firstname VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`,
+            lastname VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`,
+            createdAt DATETIME DEFAULT \'CURRENT_TIMESTAMP(6)\' NOT NULL,
+            updatedAt DATETIME DEFAULT \'CURRENT_TIMESTAMP(6)\' NOT NULL,
+            PRIMARY KEY (id)
+        ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
+    
+        $this->addSql('DROP TABLE IF EXISTS user');
     }
+    
 }
