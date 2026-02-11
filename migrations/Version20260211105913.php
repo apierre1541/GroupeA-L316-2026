a3dc51182a13abf72a55496e511be9be9a37a8bc
Youssef Badri <badriyoussef75@gmail.com>
2026-02-12 00:20:36 +0100

    fix des migrations pour eviter la duplication des foreign keys et mettre les tables idempotent

--- migrations/Version20260211105913.php
+++ migrations/Version20260211105913.php
@@ -19,21 +19,13 @@ final class Version20260211105913 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE presentation_entreprise (id INT AUTO_INCREMENT NOT NULL, logo VARCHAR(255) NOT NULL, image VARCHAR(255) NOT NULL, service_proposee VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, telephone INT NOT NULL, adresse_postal VARCHAR(255) NOT NULL, code_postal INT NOT NULL, commune VARCHAR(255) NOT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
-        $this->addSql('ALTER TABLE actualite_categorie ADD CONSTRAINT FK_EC878E9DA2843073 FOREIGN KEY (actualite_id) REFERENCES actualite (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE actualite_categorie ADD CONSTRAINT FK_EC878E9DBCF5E72D FOREIGN KEY (categorie_id) REFERENCES categorie (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE commentaire ADD CONSTRAINT FK_67F068BCA2843073 FOREIGN KEY (actualite_id) REFERENCES actualite (id)');
-        $this->addSql('ALTER TABLE signaler_commentaire ADD CONSTRAINT FK_EEF92385BA9CD190 FOREIGN KEY (commentaire_id) REFERENCES commentaire (id)');
     }
+    
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE presentation_entreprise');
-        $this->addSql('ALTER TABLE actualite_categorie DROP FOREIGN KEY FK_EC878E9DA2843073');
-        $this->addSql('ALTER TABLE actualite_categorie DROP FOREIGN KEY FK_EC878E9DBCF5E72D');
-        $this->addSql('ALTER TABLE commentaire DROP FOREIGN KEY FK_67F068BCA2843073');
-        $this->addSql('ALTER TABLE signaler_commentaire DROP FOREIGN KEY FK_EEF92385BA9CD190');
+        $this->addSql('DROP TABLE IF EXISTS presentation_entreprise');
     }
+    
 }
